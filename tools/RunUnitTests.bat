615d5364961ad3bd4648b0c077ff7079f69410f9
kostyden <denys.kostynchuk@pp.inet.fi>
2017-08-04 23:43:42 +0300

    Removed "Running in..." output while executing tests, because nunit-console will printout path of executed assemblies

--- tools/RunUnitTests.bat
+++ tools/RunUnitTests.bat
@@ -7,19 +7,13 @@ SET runner=..\packages\NUnit.ConsoleRunner.3.7.0\tools\nunit3-console.exe
 SET debug=..\UnitConverter.Tests\bin\Debug\UnitConverter.Tests.dll
 SET release=..\UnitConverter.Tests\bin\Release\UnitConverter.Tests.dll
 
-@IF EXIST %debug% echo Running tests in %debug%
 @IF EXIST %debug% %runner% %debug%
-
-@IF EXIST %release% echo Running tests in %release%
 @IF EXIST %release% %runner% %release%
 
 SET debugx86=..\UnitConverter.Tests\bin\x86\Debug\UnitConverter.Tests.dll
 SET releasex86=..\UnitConverter.Tests\bin\x86\Release\UnitConverter.Tests.dll
 
-@IF EXIST %debugx86% echo Running tests in %debugx86%
 @IF EXIST %debugx86% %runner% %debugx86%
-
-@IF EXIST %releasex86% echo Running tests in %releasex86%
 @IF EXIST %releasex86% %runner% %releasex86%
 
 @pause
