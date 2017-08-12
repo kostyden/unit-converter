b02ea89454469306c856e83876d6ab4111c8cd53
kostyden <denys.kostynchuk@pp.inet.fi>
2017-08-12 21:00:51 +0300

    Original behaviour completely reimplemented

--- UnitConverter/Program.cs
+++ UnitConverter/Program.cs
@@ -2,18 +2,23 @@
 namespace UnitConverter
 {
     using System;
-    using System.Linq;
     using System.Windows.Forms;
+    using UnitConverter.Converters;
 
     static class Program
     {
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            var converters = Enumerable.Empty<IUnitConverter>();
+            var converters = new IUnitConverter[]
+            {
+                new YardsToMetersConverter("Yards to Meters"),
+                new InchesToCentimetersConverter("Inches To Centimeters")
+            };
             var formatter = new TextFormatter();
             var mainViewModel = new MainViewModel(formatter, converters);
 
